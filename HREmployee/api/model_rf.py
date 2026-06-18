84d5cd2c0f0a8bd9f994d6dd649fd1a1979079d8
Neo <mrrneo@outlook.com>
2026-06-18 17:24:05 +0600

    ...

--- HREmployee/api/model_rf.py
+++ HREmployee/api/model_rf.py
@@ -1,15 +1,14 @@
-from fastapi import APIRouter
-from hr_employee.db.schema import EmployeeSchema
 import joblib
+import pandas as pd
 from pathlib import Path
-
-
+from fastapi import APIRouter
+from db.schema import EmployeeSchema
+from db.columns import columns
 
 BASE_DIR = Path(__file__).parent.parent
 
-model = joblib.load(BASE_DIR / 'model_rf.pkl')
-scaler = joblib.load(BASE_DIR / 'scaler.pkl')
-
+model = joblib.load(BASE_DIR / 'model_rf_HR_Employee.pkl')
+scaler = joblib.load(BASE_DIR / 'scaler_HR_Employee.pkl')
 
 predict_rf_router = APIRouter(prefix='/predict_rf', tags=['Predict_RF'])
 
@@ -17,21 +16,27 @@ predict_rf_router = APIRouter(prefix='/predict_rf', tags=['Predict_RF'])
 async def check_rf(data: EmployeeSchema):
     data_dict = dict(data)
 
-    new_job_role = data_dict.pop('JobRole')
-    job_role_binary = [
-        1 if new_job_role == 'Human Resources' else 0,
-        1 if new_job_role == 'Laboratory Technician' else 0,
-        1 if new_job_role == 'Manager' else 0,
-        1 if new_job_role == 'Manufacturing Director' else 0,
-        1 if new_job_role == 'Research Director' else 0,
-        1 if new_job_role == 'Research Scientist' else 0,
-        1 if new_job_role == 'Sales Executive' else 0,
-        1 if new_job_role == 'Sales Representative' else 0]
-
+    # 1. Бинаризация OverTime
     new_over_time = data_dict.pop('OverTime')
-    over_time_binary = [1 if new_over_time == 'Yes' else 0]
+    data_dict['OverTime'] = 1 if new_over_time == 'Yes' else 0
 
-    features = list(data_dict.values()) + job_role_binary + over_time_binary
-    scaled = scaler.transform([features])
-    prediction = model.predict(scaled)[0]
-    return {"Answer: " + 'Yes' if prediction == 1 else 'No'}
+    # 2. One-Hot Encoding для JobRole
+    new_job_role = data_dict.pop('JobRole')
+    roles = [
+        'Human Resources', 'Laboratory Technician', 'Manager',
+        'Manufacturing Director', 'Research Director', 'Research Scientist',
+        'Sales Executive', 'Sales Representative'
+    ]
+    for role in roles:
+        data_dict[f'JobRole_{role}'] = 1 if new_job_role == role else 0
+
+    data_dict['Attrition_num'] = 0
+
+    full_features_dict = {col: data_dict.get(col, 0) for col in columns}
+
+    df_input = pd.DataFrame([full_features_dict], columns=columns)
+
+    scaled = scaler.transform(df_input)
+    prediction = model.predict(scaled)
+
+    return {"Attrition": "Сотрудник, скорее всего, уволится." if prediction[0] == 1 else "Сотрудник, скорее всего, останется в компании."}
